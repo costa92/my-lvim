6006d96eb9d2c2deb49d017e1159d4f4de438ecd
costa92 <944310536@qq.com>
2024-03-31 22:06:51 +0800

    fix: lvim init

---
+++ lua/user/key_bindings.lua
@@ -0,0 +1,9 @@
+local M = {}
+-- bufferline config --
+local bufferline_config = function()
+end
+
+M.config = function()
+    bufferline_config()
+end
+return M
---
+++ lv-settings.lua
@@ -0,0 +1,16 @@
+lvim.leader = "space"
+
+-- lvim.lsp.automatic_servers_installation = true
+lvim.lsp.installer.setup.ensure_installed = {}
+
+lvim.builtin.cmp.completion.keyword_length = 2
+lvim.builtin.telescope.defaults.layout_config.width = 0.95
+lvim.builtin.telescope.defaults.layout_config.preview_cutoff = 75
+lvim.builtin.treesitter.ensure_installed = {
+  "go",
+  "gomod",
+}
+
+lvim.builtin.alpha.active = true
+lvim.builtin.dap.active = true 
+lvim.builtin.treesitter.matchup.enable = true
